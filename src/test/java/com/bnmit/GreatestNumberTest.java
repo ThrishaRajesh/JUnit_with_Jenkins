47234e5784cf6e29cc908e2e5f52b10487a28c3a
Thrisha Rajesh <23cse095@bnmit.in>
2026-04-03 20:32:26 +0530

    To test Build Failure

--- src/test/java/com/bnmit/GreatestNumberTest.java
+++ src/test/java/com/bnmit/GreatestNumberTest.java
@@ -8,6 +8,6 @@ public class GreatestNumberTest {
     @Test
     void testGreatest() {
         GreatestNumber obj = new GreatestNumber();
-        assertEquals(10, obj.findGreatest(10, 5));
+        assertEquals(5, obj.findGreatest(5, 10));
     }
 }
